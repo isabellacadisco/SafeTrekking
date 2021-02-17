1d66e3cfaada7b251d6e46cd67c8c94b73fd51a7
isabellacadisco <isabella.cadisco@gmail.com>
2021-02-17 18:44:38 +0100

    splash screen with laucher theme

--- app/src/main/java/com/example/safetrekking/MainActivity.java
+++ app/src/main/java/com/example/safetrekking/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.safetrekking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private void initData(){
+        Log.d("main", "init data");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("main", "oncreate");
+
+        initData();
+        setTheme(R.style.Theme_AppCompat_DayNight);
+        Log.d("main", "launcher");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("main", "setCV");
+
+
     }
+
+
 }
